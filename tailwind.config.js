7d7d38f8a056fc55f7a278acffd589102217158c
fabricio-alves <fabricioalves.fao@gmail.com>
2025-04-08 13:29:32 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
